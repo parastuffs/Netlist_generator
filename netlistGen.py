541fadb69b90f1cf8755b71deb3c50a13bc3ba89
Quentin Delhaye <parastuffs@gmail.com>
2021-02-01 17:29:11 +0100

    generate basic list of instances, without connectivity.

--- netlistGen.py
+++ netlistGen.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 """
 Usage:
-    netlistGen.py [--lef=<LEF file>] [--gates=n]
+    netlistGen.py (--lef=<file>) [--gates=n] [--name=<name>] [--dist=<file>]
     netlistGen.py (--help|-h)
 
 Options:
-    --lef=<LEF file>        .lef file
+    --lef=<file>            .lef file
     --gates=n               Amount of gate in the netlist
+    --name=<name>           Top module name
+    --dist=<file>           Std cell distribution file
     -h --help               Print this help
+
+Note: Power pins are ignored in standard cells.
 """
 
 from PIL import Image
@@ -38,7 +42,7 @@ logger = logging.getLogger('default')
 
 class StdCell:
     def __init__(self, name):
-        self.name = ""
+        self.name = name
         self.pins = dict() # {name : Pin instance}
         self.width = 0
         self.height = 0
@@ -58,10 +62,34 @@ class StdCell:
     def setHeight(self, h):
         self.height = h
 
-
 class Pin:
     def __init__(self,name):
         self.name = name
+        self.dir = "" # input, output, inout
+
+class Instance:
+    def __init__(self, name, cell=None):
+        self.name = name # [str] name of the instance
+        self.cell = cell # [StdCell]
+
+    def setStdCell(self, cell):
+        """
+        Parameters:
+        -----------
+        cell : StdCell
+        """
+        self.cell = cell
+
+class Net:
+    def __init__(self):
+        self.name = ""
+        self.dir = "" # input, output, wire
+
+class Netlist:
+    def __init__(self, topmodule):
+        self.topmodule = topmodule # [str] name of the to module
+        self.pins = list() # [Pin] list of input/output pins of the top module
+        self.instances = list() # [Instance]
 
         ##       ##     ###     ########   ##      ##          
         ###     ###    ## ##       ##      ###     ##          
@@ -86,10 +114,10 @@ def parseLEF(leffile):
     stdCells : dict
         dictionary of StdCell intances
     """
-    pinBlock = False # True if we are in a PIN block.
-    macroBlock = False # True if we are in a MACRO block.
 
     stdCells = dict() # Dictionary of Macro objects. Key: macro name.
+    pinBlock = False # True if we are in a PIN block.
+    macroBlock = False # True if we are in a MACRO block.
 
     with open(leffile, 'r') as f:
         lines = f.readlines()
@@ -98,7 +126,10 @@ def parseLEF(leffile):
         for line in lines:
             line = line.strip()
             if 'PIN' in line:
-                pin = Pin(line.split()[1]) # Create a Pin object. The name of the pin is the second word in the line 'PIN ...'
+                pinName = line.split()[1]
+                if "VCC" in pinName or "VSS" in pinName or "GND" in pinName or "VDD" in pinName:
+                    continue
+                pin = Pin(pinName) # Create a Pin object. The name of the pin is the second word in the line 'PIN ...'
                 stdCell.addPin(pin)
                 # print "Added the pin '"+str(pin.name)+"' to the macro '"+str(macro.name)+"'."
 
@@ -113,20 +144,83 @@ def parseLEF(leffile):
                 stdCell.setWidth(float(size[1]))
                 stdCell.setHeight(float(size[3]))
             bar()
+    return stdCells
+
+
+def generateNetlist(name, stdCells):
+    """
+
+    Parameters:
+    -----------
+    name : str
+        Name of the top module
+    stdCells : dict
+        Dictionary of StdCell objects
+
+    return Netlist instance
+    """
+    netlist = Netlist(name)
+
+    for i in range(10):
+        cell = random.choice(list(stdCells.values()))
+        name = cell.name.lower() + "_" + str(i)
+        instance = Instance(name, cell=cell)
+
+        netlist.instances.append(instance)
+
+    return netlist
+
+
+
+def writeNetlist(netlist):
+    """
+
+    Parameters:
+    -----------
+    netlist : Netlist
+    """
+
+    #############################
+    # First: top module and pins.
+    outStr = "module {} ( {} );\n".format(netlist.topmodule, ",".join([x.name for x in netlist.pins]))
+    for pin in netlist.pins:
+        outStr += "{} {};\n".format(pin.dir, pin.name)
+
+    ################
+    # Nets and wires
+
+    ###########
+    # Instances
+    for instance in netlist.instances:
+        print(instance.cell.name)
+        outStr += "{} {} ( {} );\n".format(instance.cell.name, instance.name, ", ".join(['.'+p+"()" for p in instance.cell.pins.keys()]))
+
+    ############
+    # Write file
+    filename = "{}.v".format(netlist.topmodule)
+    with open(filename, 'w') as f:
+        f.write(outStr)
+
 
 
 
 if __name__ == "__main__":
 
+    random.seed(RANDOM_SEED)
+
     args = docopt(__doc__)
+
+    topModuleName = "customDesign"
 
     if args["--lef"]:
         leffile = args["--lef"]
+    if args["--name"]:
+        topModuleName = args["--name"]
 
 
     # Create the directory for the output.
     rootDir = os.getcwd()
-    output_dir = rootDir + "/" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + "_" + leffile.split(os.sep)[-1]
+    output_dir = rootDir + "/" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + "_" + topModuleName + "_" + leffile.split(os.sep)[-1]
 
     try:
         os.makedirs(output_dir)
@@ -153,6 +247,9 @@ if __name__ == "__main__":
     stdCells = dict() # {name : StdCell instance}
 
     stdCells = parseLEF(leffile)
+
+    netlist = generateNetlist(topModuleName, stdCells)
+    writeNetlist(netlist)
 
 
     logger.info("End of all.")
